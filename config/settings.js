cec5629e978adf95e7c288a53a976fd5496b7f13
Jennifer Cuenca <>
2018-05-12 14:48:26 -0400

    installed passport and bcrypt-nodejs for authetication and encryption// setup passport and settings configs

---
+++ config/settings.js
@@ -0,0 +1,7 @@
+'use strict'
+
+if (process.env.passportSecret.length < 1) throw 'Please run \'export passportSecret=<secret>\' from the login shell';
+
+module.exports = {
+  'secret': process.env.passportSecret
+};
