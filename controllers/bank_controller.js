cbe28c8893d73b0cd5694981bd9d8bea2a527d92
Manoj Kutty <manu_kutty@yahoo.com>
2018-05-15 21:30:20 -0400

    added route to get to home page

--- controllers/bank_controller.js
+++ controllers/bank_controller.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
+
 // Import the model (customer.js) to use its database functions.
 
 var db = require("../models");
@@ -14,6 +15,30 @@ router.get("/",function(req, res) {
    res.sendFile(process.cwd() + "/public/assets/html/login.html");
 });
 
+
+// get user login id and check against the database
+router.post("/registeredCustomer", function(req, res) {
+
+  db.Customer.findOne({
+      where: {customer_email: req.body.customer_email}
+  }).then(function(dbCustomers) { 
+
+      if (dbCustomers.customer_password === req.body.customer_password)
+        {
+
+           res.sendFile(process.cwd() + "/public/assets/html/home.html");
+          // res.json(dbCustomers);
+
+        } 
+          else 
+        { 
+          res.send('Please make sure your a resistered users & your creditials are ok !');   
+        }
+    }); 
+
+
+
+
 // findAll returns all entries for a table when used with no options
 router.get("/customers", function(req, res) {
 if(req.isAuthenticated()){
@@ -25,6 +50,12 @@ else {
   res.redirect('/');
 }
  });
+
+
+
+  });
+
+
 
 
 module.exports = router;
